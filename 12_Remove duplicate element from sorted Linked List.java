f4309eca6734a3cdcb26b77d8aac0c11f79e4641
Sakshi Milind Gome <112328520+sakshi-0009@users.noreply.github.com>
2024-03-13 18:15:18 +0530

    Remove duplicate element from sorted Linked List

---
+++ 12_Remove duplicate element from sorted Linked List.java
@@ -0,0 +1,34 @@
+/* Problem Statement :-
+Given a singly linked list consisting of N nodes. The task is to remove duplicates (nodes with duplicate values) from the given list (if exists).
+Note: Try not to use extra space. The nodes are arranged in a sorted way.
+*/
+
+// Answer :-
+
+class GfG
+{
+    static Node removeDuplicates(Node head) {
+        if (head == null || head.next == null)
+            return head;
+
+        Node current = head;
+        while (current != null && current.next != null) {
+            if (current.data == current.next.data) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+        }
+        return head;
+    }
+
+    // Function to print the linked list
+    static void printList(Node head) {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+}
