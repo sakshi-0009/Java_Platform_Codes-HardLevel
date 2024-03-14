682cce794716b491b345af951aaa1df66321892c
Sakshi Milind Gome <112328520+sakshi-0009@users.noreply.github.com>
2024-03-14 23:58:48 +0530

    Given a linked list of 0s, 1s and 2s, sort it

--- 17_Given a linked list of 0s, 1s and 2s, sort it.java
+++ 17_Given a linked list of 0s, 1s and 2s, sort it.java
@@ -3,7 +3,7 @@ Given a linked list of N nodes where nodes can contain values 0s, 1s, and 2s onl
 The task is to segregate 0s, 1s, and 2s linked list such that all zeros segregate to head side, 2s at the end of the linked list, and 1s in the mid of 0s and 2s.
 */
 
-// Answer :-
+// Answer:-
 
 class Solution {
     static Node segregate(Node head) {
