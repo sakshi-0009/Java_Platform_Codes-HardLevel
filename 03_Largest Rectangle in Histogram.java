363913246372cb70b449ddef0d3ba50e9ae3f1b7
Sakshi Milind Gome <112328520+sakshi-0009@users.noreply.github.com>
2024-03-18 08:40:49 +0530

    Largest Rectangle in Histogram

---
+++ 03_Largest Rectangle in Histogram.java
@@ -0,0 +1,26 @@
+/* Problem Statement :-
+Given an array of integers heights representing the histogram's bar height where the width of each bar is 1, return the area of the largest rectangle in the histogram.
+*/
+
+// Answer :-
+
+class Solution {
+    public int largestRectangleArea(int[] heights) {
+        int n = heights.length;
+        Stack<Integer> stack = new Stack<>();
+        int maxArea = 0;
+
+        for (int i = 0; i <= n; i++) {
+            int height = (i == n) ? 0 : heights[i];
+
+            while (!stack.isEmpty() && height < heights[stack.peek()]) {
+                int h = heights[stack.pop()];
+                int w = stack.isEmpty() ? i : i - stack.peek() - 1;
+                maxArea = Math.max(maxArea, h * w);
+            }
+            stack.push(i);
+        }
+
+        return maxArea;
+    }
+}
