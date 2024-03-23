61d569b9c268e1f611027e664eef73ee9e58b791
Sakshi Milind Gome <112328520+sakshi-0009@users.noreply.github.com>
2024-03-23 22:10:46 +0530

    Palindrome Linked List

---
+++ 38_Palindrome Linked List.java
@@ -0,0 +1,32 @@
+/* Problem Statement :-
+Given the head of a singly linked list, return true if it is a palindrome or false otherwise.
+*/
+
+// Answer :-
+
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+         ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null&&fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        ListNode prev=null;
+        while(slow!=null){
+            ListNode current=slow;
+            slow=slow.next;
+            current.next=prev;
+            prev=current;
+        }
+        ListNode start=head;
+        while(start!=null&&prev!=null){
+            if (start.val!=prev.val){
+                return false;
+            }
+            start=start.next;
+            prev=prev.next;
+        }
+        return true;
+    }
+}
