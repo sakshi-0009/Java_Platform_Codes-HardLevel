970fe0c61bf9e9055cae915dfb90d0559d10edea
Sakshi Milind Gome <112328520+sakshi-0009@users.noreply.github.com>
2024-04-14 23:44:57 +0530

    Remove All Adjacent Duplicates In String

--- 40_Remove All Adjacent Duplicates In String.java
+++ 40_Remove All Adjacent Duplicates In String.java
@@ -3,7 +3,7 @@ You are given a string s consisting of lowercase English letters. A duplicate re
 We repeatedly make duplicate removals on s until we no longer can. Return the final string after all such duplicate removals have been made. It can be proven that the answer is unique.
 */
 
-// Answer :-
+// Answer:-
 
 class Solution {
     public String removeDuplicates(String s) {
